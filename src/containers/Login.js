aae003ac9912c87dacf7c00b1c16a4cf14111e44
Ariel Herman <aeh.herman@gmail.com>
2018-01-28 13:33:53 -0500

    add material ui, create basic login form

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,16 +1,84 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import MenuItem from "material-ui/Menu/MenuItem";
+import TextField from "material-ui/TextField";
+import Button from "material-ui/Button";
 // import * as actions from "../actions";
 
 class Login extends Component {
+  state = {
+    name: "",
+    password: "",
+    newName: "",
+    newPassword: "",
+    passwordConf: ""
+  };
+  handleChange = name => event => {
+    this.setState({
+      [name]: event.target.value
+    });
+  };
+
   render() {
     console.log("%c >> Inside Login \n", "color: #bada55");
-    // console.log("USER ID: ", this.props.userId);
+    console.log("STATE: ", this.state);
     // console.log("LOGGED IN: ", this.props.loggedIn);
     // console.log("PROPS: ", this.props);
     console.log("---------------------");
 
-    return <div className="login">This is the login page</div>;
+    return (
+      <div>
+        <form>
+          <TextField
+            id="name"
+            label="Name"
+            value={this.state.name}
+            onChange={this.handleChange("name")}
+          />
+          <br />
+          <TextField
+            id="password"
+            label="Password"
+            type="password"
+            value={this.state.password}
+            onChange={this.handleChange("password")}
+          />
+          <br />
+          <Button raised color="primary">
+            Login
+          </Button>
+        </form>
+
+        <form>
+          <TextField
+            id="newName"
+            label="Name"
+            value={this.state.newName}
+            onChange={this.handleChange("newName")}
+          />
+          <br />
+          <TextField
+            id="newPassword"
+            label="Password"
+            type="password"
+            value={this.state.password}
+            onChange={this.handleChange("newPassword")}
+          />
+          <br />
+          <TextField
+            id="passwordConf"
+            label="Confirm Password"
+            type="password"
+            value={this.state.passwordConf}
+            onChange={this.handleChange("passwordConf")}
+          />
+          <br />
+          <Button raised color="primary">
+            Signup
+          </Button>
+        </form>
+      </div>
+    );
   }
 }
 
