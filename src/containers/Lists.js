83ab47c2be299e6875ad95869177de3ae1945a84
Ariel Herman <aeh.herman@gmail.com>
2018-01-28 20:35:22 -0500

    add logout functionality

--- src/containers/Lists.js
+++ src/containers/Lists.js
@@ -1,10 +1,16 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import withAuth from "../hocs/withAuth";
+import * as actions from "../actions";
+import Button from "material-ui/Button";
 
 // import * as actions from "../actions";
 
 class Lists extends Component {
+  handleLogout = () => {
+    this.props.logoutUser();
+  };
+
   render() {
     console.log("%c >> Inside Lists \n", "color: #bada55");
     // console.log("USER ID: ", this.props.userId);
@@ -12,7 +18,12 @@ class Lists extends Component {
     // console.log("PROPS: ", this.props);
     console.log("---------------------");
 
-    return <div className="lists">This is the lists page</div>;
+    return (
+      <div className="lists">
+        This is the lists page
+        <Button onClick={this.handleLogout}>Logout</Button>
+      </div>
+    );
   }
 }
 
