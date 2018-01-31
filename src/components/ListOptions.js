48eff94ddd77ead642b6e9a7fa2a3c29d90d3bc2
Ariel Herman <aeh.herman@gmail.com>
2018-01-30 20:20:42 -0500

    add key to remove error in devtools

--- src/components/ListOptions.js
+++ src/components/ListOptions.js
@@ -7,8 +7,8 @@ class ListOptions extends Component {
   renderLists = () => {
     const { lists, handleSelect, handleDelete } = this.props;
     return lists.map(list => (
-      <Grid item>
-        <Card key={list.id} onClick={() => handleSelect(list)}>
+      <Grid item key={list.id}>
+        <Card onClick={() => handleSelect(list)}>
           {list.name}{" "}
           <Button onClick={() => handleDelete(list.id)}>Delete</Button>
         </Card>
